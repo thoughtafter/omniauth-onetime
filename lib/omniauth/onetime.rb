43005d6557e2ee619c777a88cf047e1f9319cbf9
thoughtafter <thoughtafter@gmail.com>
2016-12-14 12:31:17 -0800

    license under GNU Lesser General Public License 3.0 https://www.gnu.org/licenses/lgpl-3.0.en.html

--- lib/omniauth/onetime.rb
+++ lib/omniauth/onetime.rb
@@ -1,3 +1,22 @@
+#
+# omniauth-onetime - An omniauth strategy for generating secure onetime passwords.
+# Copyright (C) 2016 thoughtafter@gmail.com
+#
+# This file is part of omniauth-onetime.
+#
+# omniauth-onetime is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# omniauth-onetime is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with omniauth-onetime.  If not, see <http://www.gnu.org/licenses/>.
+#
 require "omniauth/onetime/version"
 
 module Omniauth
