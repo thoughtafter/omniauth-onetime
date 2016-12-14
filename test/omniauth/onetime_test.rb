bd7c994cdd3e167b479baa425115e1d2688193c2
thoughtafter <thoughtafter@gmail.com>
2016-12-14 13:51:21 -0800

    rubocop cleanup of tests

--- test/omniauth/onetime_test.rb
+++ test/omniauth/onetime_test.rb
@@ -1,11 +1,13 @@
 require 'test_helper'
 
-class Omniauth::OnetimeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Omniauth::Onetime::VERSION
-  end
+module OmniAuth
+  class OnetimeTest < Minitest::Test
+    def test_that_it_has_a_version_number
+      refute_nil ::Omniauth::Onetime::VERSION
+    end
 
-  def test_it_does_something_useful
-    assert false
+    def test_it_does_something_useful
+      assert false
+    end
   end
 end
