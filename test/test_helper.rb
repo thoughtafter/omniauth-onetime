2f63d2749c38cd1dff2d7cf181ae9fed5e68766e
thoughtafter <thoughtafter@gmail.com>
2021-03-21 16:39:29 -0700

    add test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+require 'simplecov-cobertura'
+
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'omniauth-onetime'
 
